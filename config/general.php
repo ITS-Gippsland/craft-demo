9ccde30e641612f50fe1ba655e290551e4c9c67d
Naveen Chanuka <support@itsgippsland.com.au>
2025-11-24 12:08:59 +0530

    Fix: Added quotes to securityKey value in general.php

--- config/general.php
+++ config/general.php
@@ -22,7 +22,7 @@ return GeneralConfig::create()
     // Prevent user enumeration attacks
     ->preventUserEnumeration()
     // Set the @webroot alias so the clear-caches command knows where to find CP resources
-    ->securityKey(pOrBXxqV+y7KtEt1pDTfvELDhPNkkh/akr1H0MEEjbA=)
+    ->securityKey('pOrBXxqV+y7KtEt1pDTfvELDhPNkkh/akr1H0MEEjbA=')
     ->aliases([
         '@webroot' => dirname(__DIR__) . '/web',
     ])
